c7e22ed24bf1f98ca52590d56ca8fd90c528abe7
RAUL DOS SANTOS ARAUJO <122099251+R4ULz@users.noreply.github.com>
2025-07-07 17:13:03 -0300

    vamos proxy

--- scrapers/testeautomacaoCaixa.py
+++ scrapers/testeautomacaoCaixa.py
@@ -138,7 +138,7 @@ def salvar_em_mongodb(imoveis, nome_collection):
 
 if __name__ == "__main__":
     options = uc.ChromeOptions()
-    options.add_argument(f'--proxy-server=http://127.0.0.1/:24000')
+    options.add_argument(f'--proxy-server=http://127.0.0.1:24000')
     options.add_argument("--window-size=1920,1080")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
